57cefd454bad75b6325f3cf4855f3fd2601f3d39
ronak-sumrani <ronak.sumrani@gmail.com>
2026-04-13 00:24:29 +0530

    Initial commit

---
+++ backend/src/routes/message.route.js
@@ -0,0 +1,9 @@
+import e from 'express';
+import express from 'express';
+const router = express.Router();
+
+router.get('/send', (req, res) => {
+  res.send('Send message endpoint');
+});
+
+export default router;
