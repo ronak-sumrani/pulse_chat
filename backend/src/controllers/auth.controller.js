3ed423defefc2f868c700f982b1d4102fec5eb27
ronak-sumrani <ronak.sumrani@gmail.com>
2026-05-14 15:45:18 +0530

    login & logout functionality added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -70,3 +70,33 @@ export const signup = async (req, res) => {
     res.status(500).json({ message: 'Internal Server error' });
   }
 };
+
+export const login = async (req, res) => {
+  const {email, password} = req.body;
+
+  try {
+    const user = await User.findOne({email});
+    if(!user) return res.status(400).json({ message: 'Invalid credentials' });
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if(!isPasswordCorrect) return res.status(400).json({ message: 'Invalid credentials' });
+
+    generateToken(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+
+  } catch (error) {
+    console.log('Error in login controller:', error);
+    res.status(500).json({ message: 'Internal Server error' });
+  }
+};
+
+export const logout = (_, res) => {
+  res.cookie("jwt","", { maxAge: 0 }); // Clear the cookie by setting it to an empty string and expiring it immediately
+  res.status(200).json({ message: 'Logged out successfully' });
+};
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,15 +1,12 @@
 import express from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { signup, login, logout } from "../controllers/auth.controller.js";
 const router = express.Router();
 
 router.post('/signup', signup);
 
-router.get('/login', (req, res) => {
-  res.send('Login endpoint');
-});
+router.post('/login', login);
 
-router.get('/logout', (req, res) => {
-  res.send('Logout endpoint');
-});
+router.post('/logout', logout);
+// logout is post request because we are modifying the server state by clearing the cookie, and it is more secure than using a GET request for logout, as GET requests can be easily triggered by malicious links or scripts, while POST requests require deliberate action from the user.
 
 export default router;
