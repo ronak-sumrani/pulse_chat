f3ec0a059ca1a0bb9e65a2a465eae484baa3f215
ronak-sumrani <ronak.sumrani@gmail.com>
2026-05-14 17:27:12 +0530

    update profile endpoint added

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+import { ENV } from "../lib/env.js";
+
+
+export const protectRoute = async(req, res, next) => {
+    try {
+        const token = req.cookies.jwt;
+        if(!token) return res.status(401).json({message: 'Unauthorized: No token provided'});
+
+        const decoded = jwt.verify(token, ENV.JWT_SECRET);
+        if(!decoded) return res.status(401).json({message: 'Unauthorized: Invalid token'});
+
+        const user = await User.findById(decoded.userId).select('-password'); // exclude password field from user object
+        if(!user) return res.status(404).json({message: 'Unauthorized: User not found'});
+
+        req.user = user; // attach user to request object for use in controllers
+
+        next(); // user is authenticated, move to update profile controller
+    } catch (error) {
+        console.error('Error in protectRoute middleware:', error);
+        res.status(500).json({message: 'Internal Server Error'});
+    }
+}
