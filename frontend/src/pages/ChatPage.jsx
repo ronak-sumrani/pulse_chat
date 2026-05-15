4cdc2f7d10364fd74335515562435439b0626ec4
ronak-sumrani <ronak.sumrani@gmail.com>
2026-05-16 02:36:39 +0530

    login & logout implemented

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-
+import { useAuthStore } from '../store/useAuthStore.js';
 function ChatPage() {
-  
+  const { logout } = useAuthStore();
   return (
-    <div>ChatPage</div>
+    <div className="z-10">
+      ChatPage
+      <button onClick={logout}>logout</button>
+      </div>
   )
 }
 
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -6,6 +6,7 @@ export const useAuthStore = create((set) => ({
     authUser: null,
     isCheckingAuth: true,
     isSigningUp: false,
+    isLoggingIn: false,
 
     checkAuth: async () => {
         try {
@@ -20,8 +21,8 @@ export const useAuthStore = create((set) => ({
     },
 
     signup : async (data) => {
+        set({isSigningUp: true});
         try {
-            set({isSigningUp: true});
             const res = await axiosInstance.post('/auth/signup', data);
             set({authUser: res.data});
 
@@ -31,5 +32,31 @@ export const useAuthStore = create((set) => ({
         } finally {
             set({isSigningUp: false});
         }
+    },
+
+    login : async (data) => {
+        set({isLoggingIn: true});
+        try {
+            const res = await axiosInstance.post('/auth/login', data);
+            set({authUser: res.data});
+
+            toast.success('Login successful! Welcome back to Pulse Chat!');
+        } catch (error) {
+            toast.error(error.response.data.message);
+        } finally {
+            set({isLoggingIn: false});
+        }
+    },
+
+    logout : async () => {
+        try {
+            await axiosInstance.post('/auth/logout');
+            set({authUser: null});
+            toast.success('Logged out successfully!');
+        } catch (error) {
+            toast.error('Error occurred while logging out.');
+            console.error('Logout error:', error);
+        }
     }
+
 }));
